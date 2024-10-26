a96bf57ed76bf677b18025256283fd32a6e39a47
Minerva <minervatbd>
2024-10-26 13:06:42 -0400

    class and attributes

---
+++ GreedyChildren.java
@@ -0,0 +1,21 @@
+/* Mina Georgoudiou
+ * Dr. Steinberg
+ * COP3503 Fall 2024
+ * Programming Assignment 3
+ */
+
+
+public class GreedyChildren
+{
+    // greedy factor of kids
+    int[] greedList;
+    // sweetness factor of candies
+    int[] sweetList;
+
+    // num of happies
+    int happyKids;
+    // num of angries
+    int angryKids;
+
+    
+}
