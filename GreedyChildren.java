b346948082f4d2c54e1dbbdd9c3766bb719c67ec
Minerva <minervatbd>
2024-10-26 15:12:44 -0400

    implemented descending merge sort

--- GreedyChildren.java
+++ GreedyChildren.java
@@ -7,7 +7,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GreedyChildren
 {
@@ -40,7 +40,14 @@ public class GreedyChildren
     // TODO
     public void greedyCandy()
     {
-        
+
+        // sort arrays via merging (nlogn runtime)
+        mergeSort(greedList, 0, kids - 1);
+        mergeSort(sweetList, 0, candies - 1);
+
+        for (int h = 0; h < 15; h++)
+                System.out.printf("%d\t%d\n", greedList[h], sweetList[h]);
+
     }
 
     public void Read(int canNum, int kidNum, String fnGreed, String fnSweet)
@@ -72,9 +79,6 @@ public class GreedyChildren
             gScan.close();
             sScan.close();
 
-            for (int h = 0; h < 15; h++)
-                System.out.printf("%d\t%d\n", greedList[h], sweetList[h]);
-
             return;
         }
         // input files werent found
@@ -89,5 +93,64 @@ public class GreedyChildren
     {
         System.out.printf("There are %d happy children.\n", this.happyKids);
         System.out.printf("There are %d angry children.\n", this.angryKids);
+    }
+
+    // for sorting in nlogn runtime
+    void mergeSort(int pData[], int l, int r)
+    {
+        // terminating condition
+        if (l >= r) return;
+        else
+        {
+
+            int mid = (l+r)/2; // midpoint
+
+            // recursive call;
+            mergeSort(pData, l, mid);
+            mergeSort(pData, mid+1, r);
+
+            // lengths of temp arrays
+            int lSize = mid - l + 1;
+            int rSize = r - mid;
+
+            // temp arrays
+            int[] lArray = new int[lSize];
+            int[] rArray = new int[rSize];
+
+            // populate temp arrays
+            for (int x = 0; x < lSize; x++)
+                lArray[x] = pData[l + x];
+            
+            for (int y = 0; y < rSize; y++)
+                rArray[y] = pData[mid+1 + y];
+
+            // put data from temp arrays back into array
+            int m = 0, n = 0; // indexes for temp arrays
+            for (int p = l; p < r+1; p++) {
+                
+                // if we've already run out of entries in rArray but lArray still has some left
+                if (n >= rSize && m < lSize) {
+                    pData[p] = lArray[m];
+                    m++;
+                }
+                // vice versa
+                else if (m >= lSize && n < rSize) {
+                    pData[p] = rArray[n];
+                    n++;
+                }
+                // otherwise, compare the two and determine which to put first
+                else {
+
+                    if (lArray[m] >= rArray[n]) {
+                        pData[p] = lArray[m];
+                        m++;
+                    }
+                    else {
+                        pData[p] = rArray[n];
+                        n++;
+                    }
+                }
+            }
+        }
     }
 }
